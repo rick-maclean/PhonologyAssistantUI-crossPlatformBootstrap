c412001fd60d82991e0e41b93494bbd372c7cfd6
Rick MacLean <maclean@dallas.sil.org>
2017-11-16 10:02:23 -0600

    state of project so Minnisota team could start to implement

---
+++ PhonologyAssistantUI-crossPlatform/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhonologyAssistantUI_crossPlatform.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PhonologyAssistant()
+        {
+            return View();
+        }
+
+        public IActionResult PhonologyAssistantFileMenu()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
